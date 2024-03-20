3243bf96a055c2832b1a6d94159d742a2c54d46f
kila1 <kila14081996@gmail.com>
2024-03-20 21:04:38 +0300

    Домашняя работа №11. Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+
+    public static void determineTheYear(int year) {
+       if (year >= 1584 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+           System.out.println(year + " Год является високосным");
+       } else {
+           System.out.println(year + " Год не является високосным");
+       }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello work 11!");
+
+        //task 1
+        System.out.println("Task 1");
+        determineTheYear(2000);
+        determineTheYear(1385);
+        determineTheYear(1997);
+
+
+
     }
 }
